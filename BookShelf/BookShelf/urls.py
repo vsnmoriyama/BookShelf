ed7d29d0ff81a948935089ab2bc50f3a0415437f
vsnpc14 <vsn_2379104@i.softbank.jp>
2019-12-04 11:14:21 +0900

    2回目

--- BookShelf/BookShelf/urls.py
+++ BookShelf/BookShelf/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
+    path('bookList/', include('bookList.urls')),
     path('admin/', admin.site.urls),
 ]
