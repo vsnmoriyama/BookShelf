83d1e84e71795e1f1f9de1dc476eff7346752fc3
vsn23kamei <vsn_2384108@i.softbank.jp>
2019-12-11 16:22:15 +0900

    CSS導入

---
+++ BookShelf/bookList/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+# Create your models here.
+class book(models.Model):                             #bookテーブル
+
+    isbn = models.CharField(max_length=200)           #isbn
+    picture_name = models.CharField(max_length=200)   #表紙
+    title = models.CharField(max_length=200)          #書名
+    author = models.CharField(max_length=200)         #著者
+    publisher = models.CharField(max_length=200)      #出版社
+    pubdate = models.DateField()      #出版日
